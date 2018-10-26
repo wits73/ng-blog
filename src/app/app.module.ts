485fa339cd936ed17a978ef9ac341866c2ea7ef5
Abraham Kim <tkim25@kennesaw.edu>
2018-10-26 15:22:23 -0400

    added ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { BlogListComponent } from './blog-list/blog-list.component';
 import { TruncatePipe } from './truncate.pipe';
 import { PaginatorComponent } from './paginator/paginator.component';
 import { HighlightDirective } from './highlight.directive';
+import { CardComponent } from './card/card.component';
+import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { HighlightDirective } from './highlight.directive';
     BlogListComponent,
     TruncatePipe,
     PaginatorComponent,
-    HighlightDirective
+    HighlightDirective,
+    CardComponent,
+    ShellComponent
   ],
   imports: [
     BrowserModule
